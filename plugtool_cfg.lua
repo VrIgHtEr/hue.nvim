ad6495a1b766a7520cd25401af12205e458fba4c
Cedric Mamo <vrighter@gmail.com>
2022-01-19 21:31:34 +0100

    updated plugtool_cfg

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -1,4 +1,23 @@
 return {
-    needs = {"vrighter/toolshed.nvim"},
-    after = {"vrighter/toolshed.nvim", 'rcarriga/nvim-notify'}
+	needs = { "vrighter/toolshed.nvim", "b0o/mapx.nvim", "nvim-telescope/telescope.nvim" },
+	after = { "vrighter/toolshed.nvim", "rcarriga/nvim-notify", "b0o/mapx.nvim", "nvim-telescope/telescope.nvim" },
+	config = {
+		{
+			function()
+				nnoremap(
+					"<leader>sS",
+					':lua require"hue/telescope".toggle_lights()()<cr>',
+					"silent",
+					"Hue: switch on and off individual lights"
+				)
+				nnoremap(
+					"<leader>ss",
+					':lua require"hue/telescope".toggle_groups()()<cr>',
+					"silent",
+					"Hue: switch on and off room lights"
+				)
+			end,
+			after = "b0o/mapx.nvim",
+		},
+	},
 }
