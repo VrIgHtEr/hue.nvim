7b23686c9763e75c47cecafe0b84e1c7d8d2dbc3
Cedric Mamo <vrighter@gmail.com>
2022-01-26 01:44:30 +0100

    Revert "updated plugtool_cfg"
    This reverts commit 938c184f1e24511637e3609313b0ab73b381b30d.

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -1,8 +1,18 @@
 return {
-    plugin_type = require('plugtool.constants').type.util,
-    needs = { 'nvim-telescope/telescope.nvim' },
-    config = function()
-        nnoremap('<leader>sS', ':lua require"hue/telescope".toggle_lights()()<cr>', 'silent', 'Hue: switch on and off individual lights')
-        nnoremap('<leader>ss', ':lua require"hue/telescope".toggle_groups()()<cr>', 'silent', 'Hue: switch on and off room lights')
-    end,
+	needs = { "nvim-telescope/telescope.nvim" },
+	after = { "nvim-telescope/telescope.nvim" },
+	config = function()
+		nnoremap(
+			"<leader>sS",
+			':lua require"hue/telescope".toggle_lights()()<cr>',
+			"silent",
+			"Hue: switch on and off individual lights"
+		)
+		nnoremap(
+			"<leader>ss",
+			':lua require"hue/telescope".toggle_groups()()<cr>',
+			"silent",
+			"Hue: switch on and off room lights"
+		)
+	end,
 }
