74b272fc9e79de52ae9338b54a82ad778fec165a
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-27 18:07:33 +0100

    started linking inventory items

--- lua/huev2/inventory.lua
+++ lua/huev2/inventory.lua
@@ -9,34 +9,56 @@ local a = require 'toolshed.async'
 
 local inventory = M.inventory
 
-local adders = {
-    light = function(_) end,
-    scene = function(_) end,
+local function find(tbl)
+    if type(tbl) == 'table' and type(tbl.rid) == 'string' and type(tbl.rtype) == 'string' and inventory[tbl.rtype] then
+        return inventory[tbl.rtype][tbl.rid]
+    end
+end
+
+local function link(x, key)
+    if type(key) ~= 'string' then
+        key = 'owner'
+    end
+    local owner = find(x[key])
+    if not owner then
+        return false
+    end
+    x[key] = owner
+    return true
+end
+
+local linkers = {
+    light = link,
+    scene = function(x)
+        link(x, 'group')
+        for _, y in ipairs(x.actions) do
+            link(y, 'target')
+        end
+    end,
     room = function(_) end,
     zone = function(_) end,
     bridge_home = function(_) end,
     grouped_light = function(_) end,
     device = function(_) end,
-    bridge = function(_) end,
-    device_power = function(_) end,
-    zigbee_connectivity = function(_) end,
+    bridge = link,
+    device_power = link,
+    zigbee_connectivity = link,
     zgp_connectivity = function(_) end,
-    motion = function(_) end,
-    temperature = function(_) end,
-    light_level = function(_) end,
+    motion = link,
+    temperature = link,
+    light_level = link,
     button = function(_) end,
     behavior_script = function(_) end,
     behavior_instance = function(_) end,
     geofence_client = function(_) end,
     geolocation = function(_) end,
     entertainment_configuration = function(_) end,
-    entertainment = function(_) end,
+    entertainment = link,
     homekit = function(_) end,
 }
 
 local function add_resource(x)
-    local adder = adders[x.type]
-    if adder then
+    if linkers[x.type] then
         local folder = inventory[x.type]
         if not folder then
             folder = {}
@@ -46,7 +68,6 @@ local function add_resource(x)
             return false
         end
         folder[x.id] = x
-        adder(x)
         return true
     else
         print('UNKNOWN RESOURCE TYPE: ' .. x.type)
@@ -58,6 +79,13 @@ local function populate_inventory(response)
     if response.data then
         for _, v in ipairs(response.data) do
             add_resource(v)
+        end
+    end
+    for _, resources in pairs(inventory) do
+        for _, x in pairs(resources) do
+            if not linkers[x.type](x) then
+                print 'ERROR!!!!!'
+            end
         end
     end
 end
@@ -85,6 +113,28 @@ a.run(function()
         return nil, 'MALFORMED_RESPONSE'
     end
     populate_inventory(response)
+
+    inventory.scene = nil
+    inventory.temperature = nil
+    inventory.zigbee_connectivity = nil
+    inventory.motion = nil
+    inventory.light_level = nil
+    inventory.light = nil
+    inventory.geolocation = nil
+    inventory.homekit = nil
+    inventory.grouped_light = nil
+    inventory.entertainment = nil
+    inventory.device_power = nil
+    inventory.behavior_script = nil
+    inventory.bridge = nil
+
+    --incomplete
+    inventory.room = nil
+    inventory.entertainment_configuration = nil
+    inventory.device = nil
+    inventory.bridge_home = nil
+    inventory.behavior_instance = nil
+    print(vim.inspect(inventory))
     return response
 end)
 
