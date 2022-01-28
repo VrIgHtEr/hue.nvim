d8b75788e53cde3347469ba47bf0e5cf1f09b1be
Cedric Mamo <vrighter@gmail.com>
2022-01-28 22:35:49 +0100

    refactor

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -2,6 +2,9 @@ return {
     needs = { 'nvim-telescope/telescope.nvim' },
     after = { 'nvim-telescope/telescope.nvim' },
     config = function()
+        nnoremap('<leader>sS', ':lua require"hue/telescope".toggle_lights()()<cr>', 'silent', 'Hue: switch on and off individual lights')
+        nnoremap('<leader>ss', ':lua require"hue/telescope".toggle_groups()()<cr>', 'silent', 'Hue: switch on and off room lights')
+
         require('huev2').subscribe('light.on', function(r, c)
             local str = 'Turned '
             if c.on then
@@ -12,15 +15,7 @@ return {
             str = str .. r.owner.metadata.name
             require('huev2.notify').log(str)
         end)
-        vim.api.nvim_exec(
-            [[augroup hue_event_close_group
-autocmd!
-autocmd VimLeave * lua require'huev2'.stop()
-augroup END]],
-            true
-        )
+        vim.api.nvim_exec("augroup hue_event_close_group\nautocmd!\nautocmd VimLeave * lua require'huev2'.stop()\naugroup END", true)
         require('huev2').start()
-        nnoremap('<leader>sS', ':lua require"hue/telescope".toggle_lights()()<cr>', 'silent', 'Hue: switch on and off individual lights')
-        nnoremap('<leader>ss', ':lua require"hue/telescope".toggle_groups()()<cr>', 'silent', 'Hue: switch on and off room lights')
     end,
 }
