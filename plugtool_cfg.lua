dc1c3ec874dc91badcf79bc042755e2052d516c2
Cedric Mamo <vrighter@gmail.com>
2022-01-29 00:01:34 +0100

    removed notification on light change

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -6,17 +6,6 @@ return {
         function()
             nnoremap('<leader>sS', ':lua require"hue/telescope".toggle_lights()()<cr>', 'silent', 'Hue: switch on and off individual lights')
             nnoremap('<leader>ss', ':lua require"hue/telescope".toggle_groups()()<cr>', 'silent', 'Hue: switch on and off room lights')
-
-            require('huev2').subscribe('light.on', function(r, c)
-                local str = 'Turned '
-                if c.on then
-                    str = str .. 'on '
-                else
-                    str = str .. 'off '
-                end
-                str = str .. r.owner.metadata.name
-                require('huev2.notify').log(str)
-            end)
             vim.api.nvim_exec("augroup hue_event_close_group\nautocmd!\nautocmd VimLeave * lua require'huev2'.stop()\naugroup END", true)
             require('huev2').start()
         end,
