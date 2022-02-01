8e67ac825acc763c2357554f3245499c7d5e02d2
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-02-01 13:58:32 +0100

    removed pixel

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -7,13 +7,11 @@ return {
             nnoremap('<leader>sS', ':lua require"huev1/telescope".toggle_lights()()<cr>', 'silent', 'Hue: switch on and off individual lights')
             nnoremap('<leader>ss', ':lua require"huev1/telescope".toggle_groups()()<cr>', 'silent', 'Hue: switch on and off room lights')
             nnoremap('<leader>sm', ':lua require"hue.map".toggle()<cr>', 'silent', 'Hue: switch on and off room lights')
-            nnoremap('<leader>sp', ':lua require"hue.pixel".toggle()<cr>', 'silent', 'Hue: switch on and off pixel display')
             vim.api.nvim_exec("augroup hue_event_close_group\nautocmd!\nautocmd VimLeave * lua require'hue'.stop()\naugroup END", true)
             require('hue').start()
 
             local rows = 30
             require('hue.map').setup { rows = rows, cols = math.floor(rows * 1.4857143) }
-            require('hue.pixel').setup { rows = 90, cols = 160 }
         end,
         {
             function()
